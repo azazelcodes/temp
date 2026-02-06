00f0783b70937afa5581cb3450aaab39fd797a0f
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2026-02-06 14:59:03 +0100

    os my custom impl + games, pls dont dmca

---
+++ binne/ruffle/README.txt
@@ -0,0 +1,10 @@
+custom ruffle build!!
+
+security.fileuri.strict_origin_policy
+has to be
+false
+in
+about:config
+
+alternatively
+python -m http.server 8000
---
+++ dl.bat
@@ -0,0 +1,6 @@
+curl -o zipped https://codeload.github.com/azazelcodes/temp/zip/refs/heads/gaming
+"C:\Program Files\7-Zip\7z.exe" x zipped
+del zipped
+del dl.bat
+
+echo user_pref("security.fileuri.strict_origin_policy", false); >> %APPDATA%\Mozilla\Firefox\Profiles\LHMS\prefs.js
