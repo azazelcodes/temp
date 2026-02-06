8da418f50a497ad84f1a15427ce2bee2825a1d76
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2026-02-06 15:16:20 +0100

    grr new batch structure

--- dl.bat
+++ dl.bat
@@ -1,6 +1,7 @@
 curl -o zipped https://codeload.github.com/azazelcodes/temp/zip/refs/heads/gaming
 "C:\Program Files\7-Zip\7z.exe" x zipped
 del zipped
-del dl.bat
+move temp-gaming/binne .
+del temp-gaming
 
 echo user_pref("security.fileuri.strict_origin_policy", false); >> %APPDATA%\Mozilla\Firefox\Profiles\LHMS\prefs.js
